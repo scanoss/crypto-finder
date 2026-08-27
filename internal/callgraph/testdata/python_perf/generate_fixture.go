8b44762d4b1c4d7829676ff938d84bdef3b81488
matiasdaloia <66310421+matiasdaloia@users.noreply.github.com>
2026-08-27 15:01:19 +0200

    docs(callgraph): complete Python KB standards audit

--- internal/callgraph/testdata/python_perf/generate_fixture.go
+++ internal/callgraph/testdata/python_perf/generate_fixture.go
@@ -1,3 +1,10 @@
+// Copyright (C) 2026 SCANOSS.COM
+// SPDX-License-Identifier: GPL-2.0-only
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; version 2.
+
 //go:build ignore
 
 // generate_fixture.go generates the synthetic Python module corpus used by
--- internal/scan/python_assigned_var_type_kb_test.go
+++ internal/scan/python_assigned_var_type_kb_test.go
@@ -1,5 +1,9 @@
 // Copyright (C) 2026 SCANOSS.COM
 // SPDX-License-Identifier: GPL-2.0-only
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; version 2.
 
 package scan
 
